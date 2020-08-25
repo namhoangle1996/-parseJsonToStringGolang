ace5b80a8a12cd1da5afc6902180a1d89138fdde
namleyolo <happybear02042000@gmail.com>
2020-08-25 18:19:58 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package  main
+
+import "encoding/json"
+
+func init()  {
+
+}
+
+
+
+
+func main()  {
+
+	go ParseJsonToString()
+}
+
+func ParseJsonToString(input json.RawMessage) string  {
+		return ""
+}
---
+++ models/cloudBuild.go
@@ -0,0 +1,9 @@
+package models
+
+type CloudBuild struct {
+	Name   string `json:"name"`
+	Id string `json:"id"`
+	ProjectId string `json:"project_id"`
+	Status string `json:"status"`
+
+}
